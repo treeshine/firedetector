90c77e13c271190f847b11c31a2bac967e67e040
LeeDongHyeuk <treeshine915@gmail.com>
2025-12-13 16:57:54 +0900

    feat: Dashboard 화면 추가 #34

--- android/app/src/main/java/com/example/fire_detector/MainActivity.kt
+++ android/app/src/main/java/com/example/fire_detector/MainActivity.kt
@@ -3,21 +3,21 @@ package com.example.fire_detector
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.fire_detector.ui.setup.UrlSetupScreen
+import com.example.fire_detector.ui.dashboard.DashboardScreen
 import com.example.fire_detector.ui.theme.Fire_detectorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             Fire_detectorTheme {
-                UrlSetupScreen(
-                    onConnectClick = { url ->
-                        println("입력된 URL: $url")
-                    }
+                DashboardScreen(
+                    dashboardUrl = "https://www.google.com",
+                    onAlarmClick = { println("알림 클릭") },
+                    onSettingClick = { println("설정 클릭") }
                 )
             }
-        }    }
+        }
+    }
 }
--- android/app/src/main/java/com/example/fire_detector/ui/theme/Theme.kt
+++ android/app/src/main/java/com/example/fire_detector/ui/theme/Theme.kt
@@ -12,32 +12,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = FireOrange,
+    secondary = FireRed
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = FireOrange,
+    secondary = FireRed
 )
 
 @Composable
 fun Fire_detectorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
